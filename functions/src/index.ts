8a6cc31ff68355a35f4530f840bcd262585a6da3
Omnomnom999 <66063943+Omnomnom999@users.noreply.github.com>
2021-06-27 18:57:33 +0800

    chore: add firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,14 @@
+import * as functions from "firebase-functions";
+import {Telegraf} from "telegraf";
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+const bot = new Telegraf(functions.config().telegrambot.key);
+
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.launch();
+
+exports.bot = functions.https.onRequest((req, res) => {
+  bot.handleUpdate(req.body, res);
+});
